524dc666920803694eade6e21bb7aef7982c28df
x-mitsui <x_mitsui@163.com>
2025-11-26 00:17:28 +0800

    add: redis配置

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { CACHE_MANAGER, Cache } from '@nestjs/cache-manager';
+import {
+  Controller,
+  Get,
+  Inject,
+  Logger,
+  Query,
+  Version,
+} from '@nestjs/common';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 @Controller()
 export class AppController {
-  constructor() {}
+  constructor(
+    @Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: Logger,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
 
   @Get()
   getHello(): string {
     return 'Hello World';
+  }
+  @Get('/token')
+  @Version('2')
+  async getCacheTest(@Query('token') token): Promise<any> {
+    this.logger.log('token:', token);
+    const res = await this.cacheManager.get('token');
+    await this.cacheManager.set('token', token || 'default token');
+    return {
+      token: res,
+    };
   }
 }
--- src/common/config/config/config.module.ts
+++ src/common/config/config/config.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule as Config } from '@nestjs/config';
+import { ConfigModule as Config, ConfigService } from '@nestjs/config';
 import Joi from 'joi';
+import { CacheableMemory } from 'cacheable';
+import { createKeyv } from '@keyv/redis';
+import { CacheModule } from '@nestjs/cache-manager';
+import Keyv from 'keyv';
 
 const envFilePath = [`.env.${process.env.NODE_ENV || 'development'}`, '.env'];
 const schema = Joi.object({
@@ -18,6 +22,31 @@ const schema = Joi.object({
       envFilePath,
       validationSchema: schema,
     }),
+    CacheModule.registerAsync({
+      imports: [Config], // 引入 ConfigModule
+      inject: [ConfigService], // 注入 ConfigService
+      useFactory: async (configService: ConfigService) => {
+        const redisURL = configService.get<string>('REDIS_URL');
+        const redisPassword = configService.get<string>('REDIS_PASSWORD');
+        const cacheTtl = configService.get<number>('CACHE_TTL');
+
+        return {
+          stores: [
+            new Keyv({
+              store: new CacheableMemory({
+                ttl: cacheTtl,
+                lruSize: 5000,
+              }),
+            }),
+            createKeyv({
+              url: redisURL,
+              password: redisPassword,
+            }),
+          ],
+        };
+      },
+    }),
   ],
+  exports: [CacheModule],
 })
 export class ConfigModule {}
